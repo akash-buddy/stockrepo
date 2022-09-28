e8f6b71ad79830f61106be9c697d5e4b880629ce
akash-buddy <114556602+akash-buddy@users.noreply.github.com>
2022-09-28 11:53:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title("Akash Prajapati")
+st.write('Hello, *World!* :sunglasses:')
+st.header("Python")
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
+st.caption('Alright!,Alright!,Alright!')
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
+df = pd.DataFrame(
+   np.random.randn(10, 5),
+   columns=('col %d' % i for i in range(5)))
+
+st.table(df)
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
