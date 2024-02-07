9ae140c3f2bc7a3248682af7a7bc9b5d3769bed2
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-02-07 14:32:33 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -131,10 +131,10 @@ trp
 
 # oppo=[]
 # titl1 = st.text_input("Enter Big moving Average --> ")
-moving_window1= st.number_input("Enter Big moving Average", value=None)
+moving_window1= st.number_input("Enter Big moving Average")
 
 # titl2 = st.text_input("Enter Small moving Average --> ")
-moving_window2=st.number_input("Enter Small moving Average", value=None)
+moving_window2=st.number_input("Enter Small moving Average")
 # print(" "*1)
 # print("#"*84)
 # print(" "*1)
