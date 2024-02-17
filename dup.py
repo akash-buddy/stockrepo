d96d5cc6373c8a34f550b12bc5d11c8a77d7afc0
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-02-17 15:38:20 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -61,45 +61,11 @@ lis=['ABBOTINDIA.NS', 'ACC.NS', 'ADANIENSOL.NS', 'ADANIGREEN.NS',
        'TORNTPHARM.NS', 'ULTRACEMCO.NS', 'UBL.NS', 'MCDOWELL-N.NS', 'UPL.NS',
        'WIPRO.NS', 'ZYDUSLIFE.NS']
 
-# sl=['Abbott India', 'ACC', 'Adani Energy Solut.', 'Adani Green Energy',
-#        'Adani Ports', 'Alkem Laboratories', 'Ambuja Cements', 'Asian Paints',
-#        'Aurobindo Pharma', 'Avenue Supermarts', 'Axis Bank', 'Bajaj Auto',
-#        'Bajaj Finance', 'Bajaj Holdings', 'Bandhan Bank', 'Bank of Baroda',
-#        'Berger Paints', 'Bharti Airtel', 'Biocon', 'Bosch', 'BPCL',
-#        'Britannia Industries', 'Cipla', 'Coal India', 'Colgate-Palmolive',
-#        'Container Corp', 'Dabur India', "Divi's Labs", 'DLF',
-#        "Dr Reddy's Labs", 'Eicher Motors', 'GAIL (India)', 'General Insurance',
-#        'Godrej Consumer', 'Grasim Industries', 'Havells India',
-#        'HCL Technologies', 'HDFC Asset Mgmt Co', 'HDFC Bank',
-#        'HDFC Life Insurance', 'Hero MotoCorp', 'Hindalco',
-#        'Hindustan Unilever', 'Hindustan Zinc', 'HPCL', 'ICICI Bank',
-#        'ICICI Lombard', 'ICICI Prudential', 'Indraprastha Gas', 'Indus Towers',
-#        'IndusInd Bank', 'Info Edge (India)', 'Infosys', 'Interglobe Aviation',
-#        'IOCL', 'ITC', 'JSW Steel', 'Kotak Mahindra Bank', 'L&T', 'LTIMindtree',
-#        'Lupin', 'Mahindra & Mahindra', 'Marico', 'Maruti Suzuki',
-#        'Muthoot Finance', 'Nestle India', 'NMDC', 'NTPC', 'OFSS', 'ONGC',
-#        'P&G', 'Petronet LNG', 'Pidilite Industries', 'Piramal Enterprises',
-#        'PNB', 'Power Finance Corp', 'Power Grid Corp','Reliance Industries','Samvardhana Motherson', 
-#        'SBI', 'SBI Cards', 'SBI Life Insurance',
-#        'Shree Cement', 'Siemens', 'Sun Pharmaceutical', 'Tata Consumer',
-#        'Tata Motors', 'TCS', 'Tech Mahindra', 'Titan', 'Torrent Pharma',
-#        'UltraTech Cement', 'United Breweries', 'United Spirits', 'UPL',
-#        'Wipro', 'Zydus']
-
-
 
 di= pd.DataFrame()
 if st.button("Refresh"):
     
     with st.spinner('Wait for few seconds.....'):
-        # start_date = '2023-01-01'
-        # end_date = datetime.now()
-        
-        # for i in lis:
-        #     data = yf.download(i, start=start_date, end=end_date)
-        #     di[sl[lis.index(i)]]=data["Close"]                # yfinn data
-        
-        
         
         dt=pd.DataFrame()
         url=f'https://groww.in/stocks/filter?closePriceHigh=100000&closePriceLow=0&index=Nifty%20100&marketCapHigh=2000000&marketCapLow=0&page=0&size=100&sortBy=COMPANY_NAME&sortType=ASC'
@@ -167,75 +133,6 @@ if st.button("Refresh"):
         st.write(f"DataFrame Length: {len(final_data)}")
         pre_data=final_data[0:len(final_data)-2]
     
-    
-    
-    
-    
-    
-        
-    
-        # oppo=[]
-        # for k in sl:
-        #     ma1 =final_data[k].rolling(moving_window1).mean()
-        #     f1=round(ma1[len(ma1)],2)
-        #     # st.write(f1)
-        #     ma2 =final_data[k].rolling(moving_window2).mean()
-        #     f2=round(ma2[len(ma1)],2)
-        #     # st.write(f2)
-        #     if (final_data.at[len(final_data),k]>=0) and (final_data.at[len(final_data),k]<=100):
-        #         if (f1 >= f2) and ((f1-f2) >= (0) and (f1-f2) <= (3) ) :
-        #             oppo.append("buy")
-        #         elif (f1 <= f2) and ((f1-f2) >= (0) and (f1-f2) <= (-3) ) :
-        #             oppo.append("sell")
-        #         else:
-        #             oppo.append("Wait for opportunity")
-        #     elif (final_data.at[len(final_data),k]>=101) and (final_data.at[len(final_data),k]<=200):
-        #         if (f1 >= f2) and ((f1-f2) >= (0) and (f1-f2) <= (3) ) :
-        #             oppo.append("buy")
-        #         elif (f1 <= f2) and ((f1-f2) >= (0) and (f1-f2) <= (-3) ) :
-        #             oppo.append("sell")
-        #         else:
-        #             oppo.append("Wait for opportunity")
-        
-        #     elif (final_data.at[len(final_data),k]>=201) and (final_data.at[len(final_data),k]<=500):
-        #         if (f1 >= f2) and ((f1-f2) >= (0) and (f1-f2) <= (3) ) :
-        #             oppo.append("buy")
-        #         elif (f1 <= f2) and ((f1-f2) >= (0) and (f1-f2) <= (-3) ) :
-        #             oppo.append("sell")
-        #         else:
-        #             oppo.append("Wait for opportunity")
-        
-        #     elif (final_data.at[len(final_data),k]>=501) and (final_data.at[len(final_data),k]<=1000):
-        #         if (f1 >= f2) and ((f1-f2) >= (0) and (f1-f2) <= (4) ) :
-        #             oppo.append("buy")
-        #         elif (f1 <= f2) and ((f1-f2) >= (0) and (f1-f2) <= (-4) ) :
-        #             oppo.append("sell")
-        #         else:
-        #             oppo.append("Wait for opportunity")
-        
-        #     elif (final_data.at[len(final_data),k]>=1001) and (final_data.at[len(final_data),k]<=2000):
-        #         if (f1 >= f2) and ((f1-f2) >= (0) and (f1-f2) <= (5) ) :
-        #             oppo.append("buy")
-        #         elif (f1 <= f2) and ((f1-f2) >= (0) and (f1-f2) <= (-5) ) :
-        #             oppo.append("sell")
-        #         else:
-        #             oppo.append("Wait for opportunity")
-        
-        #     elif (final_data.at[len(final_data),k]>=2001) and (final_data.at[len(final_data),k]<=5000):
-        #         if (f1 >= f2) and ((f1-f2) >= (0) and (f1-f2) <= (10) ) :
-        #             oppo.append("buy")
-        #         elif (f1 <= f2) and ((f1-f2) >= (0) and (f1-f2) <= (-10) ) :
-        #             oppo.append("sell")
-        #         else:
-        #             oppo.append("Wait for opportunity")
-        
-        #     else :
-        #         if (f1 >= f2) and ((f1-f2) >= (0) and (f1-f2) <= (15) ) : 
-        #             oppo.append("buy")
-        #         elif (f1 <= f2) and ((f1-f2) >= (0) and (f1-f2) <= (-15) ) :
-        #             oppo.append("sell")
-        #         else:
-        #             oppo.append("Wait for opportunity")
     
         oppo=[]
         for i in sl:
@@ -316,24 +213,8 @@ if st.button("Refresh"):
             stocks=dt[dt["Recommended"]=="Wait for opportunity"]
             st.dataframe(stocks, use_container_width=True)
 
-    st.success('Done!') 
-# print(" "*1)
-# print(" 1 👈 For Buying stocks ")
-# print(" 2 👈 For Selling stocks ")
-# print(" 3 👈 For Opportunity stocks ")
-# print(" "*1)
-# print("#"*84)
-# print(" "*1)
-# cek=int(input("Choose number to see stocks -->"))
-# print(" "*1)
-# print("#"*84)
-# print(" "*1)
-# if cek==1:
-#     stocks=gdf[gdf["Recommended"]=="buy"]
-# elif cek==2:
-#     stocks=gdf[gdf["Recommended"]=="sell"]
-# else:
-#     stocks=gdf[gdf["Recommended"]=="Wait for opportunity"]
+    # st.success('Done!') 
+
     
     
 
