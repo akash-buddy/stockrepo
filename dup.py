565d1ad42fc807289924a32dd854a921af5aee70
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-02-28 23:56:59 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -823,7 +823,7 @@ if option=="Banking":
             Price=[]
             change_price=[]
             t=0
-            for i in s[2:]:
+            for i in s[1:]:
                 N=i.find_all('span',class_="st76SymbolName")
                 P=(i.find_all('div',class_="st76CurrVal bodyBaseHeavy"))[0].text
                 removequma=P.replace(",","")
