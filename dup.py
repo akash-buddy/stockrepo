5b0698efaf67bd39b5ff4a2fceea69e4d8ae6017
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-03-19 00:10:05 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -110,13 +110,12 @@ if option=="Nifty 100":
             for i in lis:
                 data = yf.download(i, start=start_date, end=end_date)
                 di[sl[lis.index(i)]]=data["Close"] 
-            di
+            
             # Concatinating both dataframe: yfin + grow
             result = pd.concat([di, trp], ignore_index=True)
-            result
             final_da=result.drop(len(result)-(len(trp)+1))
             final_data=final_da.fillna(0)
-            st.dataframe(final_data)
+            # st.dataframe(final_data)
             # st.write(f"DataFrame Length: {len(final_data)}")
             pre_data=final_data[0:len(final_data)-len(trp)]
     
