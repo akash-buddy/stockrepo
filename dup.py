064a260d930a3b09ac9627d679951e4b77e935e5
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-03-19 23:43:18 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -297,7 +297,11 @@ if option=="Nifty 100":
                         
                         st.metric(label=o1, value=f"₹{o2}", delta=f"{o3}₹")
                         st.markdown(f'[Click here to Invest](https://groww.in/charts/stocks/{grow_link}?exchange=NSE)')
-                     
+
+                    with com2:
+                        st.header('India's Paytm likely to partner with four banks for enabling UPI transactions, sources say', divider='rainbow')
+                        st.markdown(f'[Read Now](https://finance.yahoo.com/news/troubled-indian-fintech-paytm-wins-154956334.html)')
+                      /news/indias-paytm-likely-partner-four-130117686.html
             # elif filtter=="Buy":
             #     stocks=dt[dt["Recommended"]=="buy"]
             #     com1, com2, com3,com4,com5 = st.columns(5)
