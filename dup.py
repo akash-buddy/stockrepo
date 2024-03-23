f352a36f31ff127af852dd68a7c6dfeea11a6a50
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-03-23 23:27:14 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -18,8 +18,7 @@ st.set_page_config(
 col17,col27,col37=st.columns([3,1,3])
 with col27:
     image=Image.open('bull.png')
-    edited=image.resize((3060,300))
-    st.image(edited,use_column_width=True)
+    st.image(image,use_column_width=True)
 
 coll1,coll2,coll3=st.columns([1.5,2,1])
 with coll2:
