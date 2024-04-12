578795759c7d1594398ce01f5c982f96e83065f5
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-04-12 18:58:30 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -110,7 +110,7 @@ if option=="Nifty 100":
             for i in lis:
                 data = yf.download(i, start=start_date, end=end_date)
                 di[sl[lis.index(i)]]=data["Close"] 
-            # di
+            di
             # Concatinating both dataframe: yfin + grow
             result = pd.concat([di, trp], ignore_index=True)
             # final_data=result.fillna(0)
@@ -122,9 +122,9 @@ if option=="Nifty 100":
             # st.dataframe(pre_data)
     
             current_price=final_data[(len(final_data)-(len(trp))):len(final_data)-(len(trp)-1)]
-            # st.write(current_price)
+            st.write(current_price)
             previous_price=final_data[(len(final_data)-(len(trp)+1)):len(final_data)-len(trp)]
-            # st.write(previous_price)
+            st.write(previous_price)
             change_price=[]
             for i in range(len(sl)):
                 change_p=current_price.iloc[0,i]-previous_price.iloc[0,i]
