2f61962b2f03cad242670925a8df62a3f993bae2
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-04-13 09:53:27 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 import pandas_datareader as web
-# from pandas_datareader import data as pdr
+from keras.models import load_model
 import yfinance as yf
 # import time
 import requests
@@ -401,6 +401,46 @@ if option=="Nifty 100":
                                 plt.plot(mov1,'r')
                                 plt.plot(df.Close,'b')
                                 st.pyplot(figg)
+                                data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+                                data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+                                
+                                from sklearn.preprocessing import MinMaxScaler
+                                scaler = MinMaxScaler(feature_range=(0,1))
+                                
+                                data_training_array = scaler.fit_transform(data_training)
+                                model = load_model('keras_model.h5')
+                                
+                                past_100_days = data_training.tail(100)
+                                final_df=past_100_days.append(data_testing,ignore_index=True)
+                                input_data = scaler.fit_transform(final_df)
+                                
+                                x_test = []
+                                y_test = []
+                                
+                                for i in range(100,input_data.shape[0]):
+                                    x_test.append(input_data[i-100:i])
+                                    y_test.append(input_data[i,0])
+                                
+                                x_test,y_test = np.array(x_test),np.array(y_test)
+                                
+                                y_predicted = model.predict(x_test)
+                                
+                                scal=scaler.scale_
+                                
+                                scale_factor = 1/scal[0]
+                                y_predicted=y_predicted*scale_factor
+                                y_test=y_test*scale_factor
+                                
+                                st.subheader("Predictions vs Original")
+                                fig2 = plt.figure(figsize=(12,6))
+                                plt.plot(y_test,'b',label='Original Price')
+                                plt.plot(y_predicted,'g',label='Predicted Price')
+                                plt.xlabel('Time')
+                                plt.ylabel('Price')
+                                st.pyplot(fig2)
+
+
+
                                 
                                 st.write(mov1)
                                 st.write(mov2)
