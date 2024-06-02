50255543ca7e207f75087f666d2887cda0149a4d
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-06-02 13:00:23 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -109,7 +109,7 @@ if option=="Nifty 100":
             start_date = '2023-06-01'
             # end_date = '2024-04-12'
             end_date = datetime.now()
-            st.dataframe(end_date)
+            st.write(end_date)
             for i in lis:
                 data = yf.download(i, start=start_date, end=end_date)
                 di[sl[lis.index(i)]]=data["Close"] 
