fa99adcc0243c19c5b8e6866f7f315308add1da9
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-06-03 17:40:56 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -107,8 +107,8 @@ if option=="Nifty 100":
             sl=trp.columns
             
             start_date = '2023-06-01'
-            # end_date = '2024-04-12'
-            end_date = datetime.now()
+            end_date = '2024-06-03'
+            # end_date = datetime.now()
             # st.write(end_date)
             for i in lis:
                 data = yf.download(i, start=start_date, end=end_date)
