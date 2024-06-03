5c63e24523a4cf3d2836a5a2481f9dacc19a426d
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-06-03 17:44:55 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -117,7 +117,7 @@ if option=="Nifty 100":
             # Concatinating both dataframe: yfin + grow
             result = pd.concat([di, trp], ignore_index=True)
             # final_data=result.fillna(0)
-            final_da=result.drop(len(result)-(len(trp)+1))
+            # final_da=result.drop(len(result)-(len(trp)+1))
             final_data=final_da.fillna(0)
             st.dataframe(final_data)
             # st.write(f"DataFrame Length: {len(final_data)}")
